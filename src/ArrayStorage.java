f0fd2b14d976e9de4b6f66c1d770bfac2b46b1d6
1nquis1tor <konstantin.gomeniuk@gmail.com>
2019-12-01 08:46:10 +0100

    changes in size, get, clear, delete function

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,21 +1,23 @@
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 
 public class ArrayStorage {
-    private Resume[] storage = new Resume[100000];
+    private Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
-        Arrays.fill(storage, null);
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
-
 
     void save(Resume r) {
         for (int i = 0; i < storage.length; i++) {
             if (storage[i] == null) {
                 storage[i] = r;
+                size++;
                 break;
             }
 
@@ -25,35 +27,33 @@ public class ArrayStorage {
     Resume get(String uuid) {
         Resume found = null;
         for (Resume resume : storage) {
-            if (resume.uuid.equals(uuid)) {
-                found = resume;
-                break;
+            if (resume != null) {
+                if (resume.uuid.equals(uuid)) {
+                    found = resume;
+                    break;
+                }
             }
-
         }
         return found;
     }
 
     void delete(String uuid) {
-
         for (int i = 0; i < storage.length; i++) {
             if (storage[i] != null) {
-                if (storage[i].uuid.equals(uuid))
+                if (storage[i].uuid.equals(uuid)) {
                     storage[i] = null;
+                    size--;
+                }
             }
-
         }
 
         for (int i = 0; i < storage.length - 2; i++) {
-
             if (storage[i] == null) {
                 storage[i] = storage[i + 1];
                 storage[i + 1] = storage[i + 2];
                 storage[i + 2] = null;
-
             }
         }
-
     }
 
 
@@ -61,24 +61,12 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-
         Resume[] getAll = new Resume[size()];
         if (size() >= 0) System.arraycopy(storage, 0, getAll, 0, size());
         return getAll;
     }
 
     int size() {
-        int length = 0;
-        if (storage[0] == null)
-            return length;
-
-        for (int i = 1; i < storage.length; i++) {
-            if (storage[i] == null)
-                return i;
-        }
-
-        return length;
+        return size;
     }
-
-
 }
