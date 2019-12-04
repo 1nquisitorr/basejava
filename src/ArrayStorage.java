50748c8f628d75aadc2303c11319de6f831e8d53
1nquis1tor <konstantin.gomeniuk@gmail.com>
2019-12-04 22:34:21 +0100

    function optimization

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,26 +3,25 @@
  */
 
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private static int ARRAYSIZE = 10000;
+    private Resume[] storage = new Resume[ARRAYSIZE];
     int size = 0;
 
     void clear() {
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < ARRAYSIZE; i++) {
             storage[i] = null;
         }
         size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                size++;
-                break;
-            }
-
+        if (size < ARRAYSIZE) {
+            storage[size] = r;
+            size++;
         }
+
     }
+
 
     Resume get(String uuid) {
         Resume found = null;
@@ -38,20 +37,12 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                if (storage[i].uuid.equals(uuid)) {
-                    storage[i] = null;
-                    size--;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int j = i; j < size; j++) {
+                    storage[j] = j < size ? storage[j + 1] : null;
                 }
-            }
-        }
-
-        for (int i = 0; i < storage.length - 2; i++) {
-            if (storage[i] == null) {
-                storage[i] = storage[i + 1];
-                storage[i + 1] = storage[i + 2];
-                storage[i + 2] = null;
+                size--;
             }
         }
     }
@@ -62,7 +53,7 @@ public class ArrayStorage {
      */
     Resume[] getAll() {
         Resume[] getAll = new Resume[size()];
-        if (size() >= 0) System.arraycopy(storage, 0, getAll, 0, size());
+        if (size() >= 0) System.arraycopy(storage, 0, getAll, 0, size);
         return getAll;
     }
 
